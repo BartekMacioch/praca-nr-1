e835cbd62117ca8b6814b6782b8a7ab04a8fc55e
Bartek Macioch <B.Macioch@wit.edu.pl>
2021-12-08 23:26:17 +0100

    zadanie 1

---
+++ zadanie1wykład2petle.py
@@ -0,0 +1,8 @@
+#zdn1
+username = 'Admin'
+password = '1234'
+x = input('podaj login')
+y = input('podaj haslo')
+while x!=username or y!=password:
+    x = input('podaj login')
+    y = input('podaj haslo')
