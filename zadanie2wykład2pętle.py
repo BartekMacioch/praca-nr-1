f79cc2e9aee7fff117f10bd0438832931be33e89
Bartek Macioch <B.Macioch@wit.edu.pl>
2021-12-08 23:28:06 +0100

    zadanie 2

---
+++ zadanie2wykład2pętle.py
@@ -0,0 +1,7 @@
+username = 'Admin'
+password = '1234'
+x = input('podaj login')
+y = input('podaj haslo')
+while x!=username or y!=password:
+    x = input('podaj login')
+    y = input('podaj haslo')
