898ded5be3ed07abb3d021e8076c3a9aef63e39d
Bartek Macioch <B.Macioch@wit.edu.pl>
2021-12-08 22:50:36 +0100

    zadanie 1

---
+++ zadanie1.py
@@ -0,0 +1,8 @@
+#zdn1
+rating = int(input('ocen swoj dzien od 0 do 10:'))
+twoj_dzien = rating/ 10
+if(rating<10 and rating>0):
+    print('ocena twojego dnia wynosi:',twoj_dzien )
+else:
+    print('liczba musi byc w przedziale od 0 do 10')
+#ocena twojego dnia wynosi: 0.2
